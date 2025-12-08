f10a8fda21af93fc2066e9644ccd7b8c48d67455
Wouter Pauwels <37985539+ochotonida@users.noreply.github.com>
2025-12-08 16:30:11 +0100

    Cleanup surface rules, fix TerraBlenderFix incompatibility

--- common/src/main/java/gardensofthedead/registry/ModSurfaceRules.java
+++ common/src/main/java/gardensofthedead/registry/ModSurfaceRules.java
@@ -6,118 +6,71 @@ import net.minecraft.world.level.levelgen.Noises;
 import net.minecraft.world.level.levelgen.SurfaceRules;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
 
+import static net.minecraft.world.level.levelgen.SurfaceRules.*;
+
+// TODO cleanup a bit more maybe
 public class ModSurfaceRules {
 
-    private static final SurfaceRules.RuleSource BEDROCK = makeStateRule(Blocks.BEDROCK);
-    private static final SurfaceRules.RuleSource GRAVEL = makeStateRule(Blocks.GRAVEL);
-    private static final SurfaceRules.RuleSource LAVA = makeStateRule(Blocks.LAVA);
-    private static final SurfaceRules.RuleSource NETHERRACK = makeStateRule(Blocks.NETHERRACK);
-    private static final SurfaceRules.RuleSource SOUL_SAND = makeStateRule(Blocks.SOUL_SAND);
-    private static final SurfaceRules.RuleSource SOUL_SOIL = makeStateRule(Blocks.SOUL_SOIL);
-    private static final SurfaceRules.RuleSource NETHER_WART_BLOCK = makeStateRule(Blocks.NETHER_WART_BLOCK);
-    private static final SurfaceRules.RuleSource CRIMSON_NYLIUM = makeStateRule(Blocks.CRIMSON_NYLIUM);
+    private static final SurfaceRules.RuleSource GRAVEL = block(Blocks.GRAVEL);
+    private static final SurfaceRules.RuleSource NETHERRACK = block(Blocks.NETHERRACK);
+    private static final SurfaceRules.RuleSource SOUL_SAND = block(Blocks.SOUL_SAND);
+    private static final SurfaceRules.RuleSource SOUL_SOIL = block(Blocks.SOUL_SOIL);
+    private static final SurfaceRules.RuleSource NETHER_WART_BLOCK = block(Blocks.NETHER_WART_BLOCK);
+    private static final SurfaceRules.RuleSource CRIMSON_NYLIUM = block(Blocks.CRIMSON_NYLIUM);
 
     public static SurfaceRules.RuleSource makeRules() {
-        SurfaceRules.ConditionSource aboveOrAtLava = SurfaceRules.yBlockCheck(VerticalAnchor.absolute(31), 0);
-        SurfaceRules.ConditionSource aboveLava = SurfaceRules.yBlockCheck(VerticalAnchor.absolute(32), 0);
-        SurfaceRules.ConditionSource above30 = SurfaceRules.yStartCheck(VerticalAnchor.absolute(30), 0);
-        SurfaceRules.ConditionSource below35 = SurfaceRules.not(SurfaceRules.yStartCheck(VerticalAnchor.absolute(35), 0));
-        SurfaceRules.ConditionSource topBedrockLayer = SurfaceRules.yBlockCheck(VerticalAnchor.belowTop(5), 0);
-        SurfaceRules.ConditionSource hole = SurfaceRules.hole();
-        SurfaceRules.ConditionSource patchNoise = SurfaceRules.noiseCondition(Noises.PATCH, -0.012D);
-        SurfaceRules.ConditionSource netherrackNoise = SurfaceRules.noiseCondition(Noises.NETHERRACK, 0.4D);
-        SurfaceRules.RuleSource gravelBeach = SurfaceRules.ifTrue(patchNoise, SurfaceRules.ifTrue(above30, SurfaceRules.ifTrue(below35, GRAVEL)));
-        SurfaceRules.ConditionSource soulBlightNetherrackNoise = SurfaceRules.noiseCondition(Noises.NETHER_STATE_SELECTOR, -0.2D);
-        SurfaceRules.ConditionSource soulBlightSoulSandNoise = SurfaceRules.noiseCondition(Noises.NETHER_STATE_SELECTOR, 0.3D);
-        SurfaceRules.ConditionSource netherWartNoise = SurfaceRules.noiseCondition(Noises.NETHER_WART, 1.17D);
+        SurfaceRules.ConditionSource aboveLava = yBlockCheck(VerticalAnchor.absolute(31), 0);
 
-        return SurfaceRules.sequence(
-                SurfaceRules.ifTrue(
-                        SurfaceRules.verticalGradient(
-                                "bedrock_floor",
-                                VerticalAnchor.bottom(),
-                                VerticalAnchor.aboveBottom(5)
-                        ),
-                        BEDROCK
-                ),
-                SurfaceRules.ifTrue(
-                        SurfaceRules.not(
-                                SurfaceRules.verticalGradient(
-                                        "bedrock_roof",
-                                        VerticalAnchor.belowTop(5),
-                                        VerticalAnchor.top()
-                                )
-                        ),
-                        BEDROCK
-                ),
-
-                SurfaceRules.ifTrue(topBedrockLayer, NETHERRACK),
-                SurfaceRules.ifTrue(SurfaceRules.isBiome(ModBiomes.SOULBLIGHT_FOREST),
-                        SurfaceRules.sequence(
-                                SurfaceRules.ifTrue(
-                                        SurfaceRules.UNDER_CEILING,
-                                        SurfaceRules.sequence(
-                                                SurfaceRules.ifTrue(soulBlightSoulSandNoise, SOUL_SAND),
-                                                SurfaceRules.ifTrue(soulBlightNetherrackNoise, SOUL_SOIL),
-                                                NETHERRACK
-                                        )
-                                ),
-                                SurfaceRules.ifTrue(
-                                        SurfaceRules.UNDER_FLOOR,
-                                        SurfaceRules.sequence(
-                                                gravelBeach,
-                                                SurfaceRules.ifTrue(soulBlightSoulSandNoise, SOUL_SAND),
-                                                SOUL_SOIL
-                                        )
-                                )
+        SurfaceRules.RuleSource soulblightForest = sequence(
+                ifTrue(
+                        UNDER_CEILING,
+                        sequence(
+                                ifTrue(noiseCondition(Noises.NETHER_STATE_SELECTOR, 0.3D), SOUL_SAND),
+                                ifTrue(noiseCondition(Noises.NETHER_STATE_SELECTOR, -0.2D), SOUL_SOIL),
+                                NETHERRACK
                         )
                 ),
-                SurfaceRules.ifTrue(SurfaceRules.isBiome(ModBiomes.SOULBLIGHT_FOREST),
-                        SurfaceRules.sequence(
-                                SurfaceRules.ifTrue(
-                                        SurfaceRules.UNDER_CEILING,
-                                        SurfaceRules.sequence(
-                                                SurfaceRules.ifTrue(soulBlightSoulSandNoise, SOUL_SAND),
-                                                SurfaceRules.ifTrue(soulBlightNetherrackNoise, SOUL_SOIL),
-                                                NETHERRACK
-                                        )
-                                ),
-                                SurfaceRules.ifTrue(
-                                        SurfaceRules.UNDER_FLOOR,
-                                        SurfaceRules.sequence(
-                                                gravelBeach,
-                                                SurfaceRules.ifTrue(soulBlightSoulSandNoise, SOUL_SAND),
-                                                SOUL_SOIL
-                                        )
-                                )
+                ifTrue(
+                        UNDER_FLOOR,
+                        sequence(
+                                gravelBeach(),
+                                ifTrue(noiseCondition(Noises.NETHER_STATE_SELECTOR, 0.3D), SOUL_SAND),
+                                SOUL_SOIL
                         )
                 ),
-                SurfaceRules.ifTrue(
-                        SurfaceRules.ON_FLOOR,
-                        SurfaceRules.sequence(
-                                SurfaceRules.ifTrue(
-                                        SurfaceRules.not(aboveLava),
-                                        SurfaceRules.ifTrue(hole, LAVA)
-                                ),
-                                SurfaceRules.ifTrue(
-                                        SurfaceRules.isBiome(ModBiomes.WHISTLING_WOODS),
-                                        SurfaceRules.ifTrue(
-                                                SurfaceRules.not(netherrackNoise),
-                                                SurfaceRules.ifTrue(aboveOrAtLava,
-                                                        SurfaceRules.sequence(
-                                                                SurfaceRules.ifTrue(netherWartNoise, NETHER_WART_BLOCK),
-                                                                CRIMSON_NYLIUM
-                                                        )
-                                                )
+                NETHERRACK
+        );
+
+        SurfaceRules.RuleSource whistlingWoods = sequence(
+                ifTrue(ON_FLOOR,
+                        ifTrue(not(noiseCondition(Noises.NETHERRACK, 0.4D)),
+                                ifTrue(aboveLava,
+                                        sequence(
+                                                ifTrue(noiseCondition(Noises.NETHER_WART, 1.17D), NETHER_WART_BLOCK),
+                                                CRIMSON_NYLIUM
                                         )
                                 )
                         )
                 ),
                 NETHERRACK
         );
+
+        return sequence(
+                ifTrue(isBiome(ModBiomes.SOULBLIGHT_FOREST), soulblightForest),
+                ifTrue(isBiome(ModBiomes.WHISTLING_WOODS), whistlingWoods)
+        );
     }
 
-    private static SurfaceRules.RuleSource makeStateRule(Block block) {
-        return SurfaceRules.state(block.defaultBlockState());
+    private static SurfaceRules.RuleSource gravelBeach() {
+        SurfaceRules.ConditionSource above30 = yStartCheck(VerticalAnchor.absolute(30), 0);
+        SurfaceRules.ConditionSource below35 = not(yStartCheck(VerticalAnchor.absolute(35), 0));
+        SurfaceRules.ConditionSource patchNoise = noiseCondition(Noises.PATCH, -0.012D);
+        return ifTrue(patchNoise,
+                ifTrue(above30, ifTrue(below35, GRAVEL))
+        );
+    }
+
+    private static SurfaceRules.RuleSource block(Block block) {
+        return state(block.defaultBlockState());
     }
 }
